d40b9b45c0da6b1f57b4ece5881afd9ff138e350
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2018-10-18 22:53:37 -0300

    autenticacao e passaporte configurado

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,18 +1,49 @@
+const admin = require('./admin')
+
 module.exports = app => {
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
-        .get(app.api.user.get)
-        .post(app.api.user.save)
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.user.get))
+        .post(admin(app.api.user.save))
 
     app.route('/users/:id')
-        .get(app.api.user.getById)
-        .put(app.api.user.save)
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.user.getById))
+        .put(admin(app.api.user.save))
 
-     app.route('/categories')
-        .get(app.api.category.get)
-        .post(app.api.category.save)
+    app.route('/categories')
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.category.get))
+        .post(admin(app.api.category.save))
+
+    // Cuidado com ordem! 
+    app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
+        .get(app.api.category.getTree)
 
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
-        .put(app.api.category.save)
-        .delete(app.api.category.remove)
+        .put(admin(app.api.category.save))
+        .delete(admin(app.api.category.remove))
+
+    app.route('/articles')
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.article.get))
+        .post(admin(app.api.article.save))
+
+    app.route('/articles/:id')
+        .all(app.config.passport.authenticate())    
+        .get(app.api.article.getById)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
+
+    app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
+        .get(app.api.article.getByCategory)
+
 }
