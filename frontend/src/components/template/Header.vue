f74ebdadaab716672cc808eaa644ac67c1e1529f
Rodrigo Frazão Maia <rodrigo.frazao@armateus.com.br>
2018-11-16 13:53:07 -0300

    criado componentes pagetitle e userdropdwon , adicionado navegacao

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -4,14 +4,18 @@
             <i class="fa fa-lg" :class="icon"></i>
         </a>
         <h1 class="title">
-            {{ title }}
+            <router-link to="/"> {{ title }} </router-link>            
         </h1>
+        <UserDropdown v-if="!hideUserDropdown" />
     </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropdown'
+
 export default {
     name: 'Header',
+    components: { UserDropdown },
     props: {
         title: String,
         hideToggle: Boolean,
