46d842ef1e26fcfb788b0a7ca5d538073c92ce71
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2018-11-16 21:43:37 -0300

    paginas administrativas criadas

---
+++ frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="user-admin">
+        <h1>Usuario componente</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserAdmin'
+
+}
+</script>
+
+<style>
+
+</style>
